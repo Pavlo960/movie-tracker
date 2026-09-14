9cfa56ded1ef1f7ade49a3751ebbfa2084c84fff
pavlo960 <kokoa260180@gmail.com>
2026-09-14 17:02:44 +0300

    Add templates inheritance, movie details and director list

--- films_config/settings.py
+++ films_config/settings.py
@@ -55,10 +55,14 @@ ROOT_URLCONF = 'films_config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'movies' / 'templates',
+            BASE_DIR / 'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Director, Movie
 
-# Create your views here.
+
+def movie_list(request):
+  movies = (
+      Movie.objects.select_related('director').prefetch_related('genre').all()
+  )
+  return render(request, 'movies/movie_list.html', {'movies': movies})
+
+
+def movie_detail(request, pk):
+  movie = get_object_or_404(Movie, pk=pk)
+  return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+
+def director_list(request):
+  directors = Director.objects.prefetch_related('movies').all()
+  return render(
+      request, 'movies/director_list.html', {'directors': directors}
+  )
